da96bfc8ccfd7d99f019c669f8ffc48a69e2520c
Nghiêm Viễn <viennt@gostream.vn>
2022-03-16 08:55:56 +0700

    fix formatMoney

--- frontend/src/helper/index.js
+++ frontend/src/helper/index.js
@@ -22,10 +22,19 @@ export default {
 		formatVREF(value) {
 	      return window.web3.utils.fromWei(value.toString());
 	    },
-	    formatMoney(price) {
-	    	let dollarUSLocale = Intl.NumberFormat('en-US');
-	    	return dollarUSLocale.format(price)
-	    },
+	    formatMoney(number, decPlaces, decSep, thouSep) {
+		    decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+		    decSep = typeof decSep === "undefined" ? "." : decSep;
+		    thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+		    var sign = number < 0 ? "-" : "";
+		    var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(20)));
+		    var j = (j = i.length) > 3 ? j % 3 : 0;
+
+		    return sign +
+		        (j ? i.substr(0, j) + thouSep : "") +
+		        i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
+		        (decPlaces ? decSep + Math.abs(number - i).toFixed(20).slice(2) : "").slice(0,decPlaces);
+		},
 	    formatUSDC(value) {
 	      return value/10**6;
 	    },
