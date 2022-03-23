530c345f040bb47f3af41d3219a5f34d2ee1256d
Thinhbp <90796376+Thinhbp@users.noreply.github.com>
2022-03-23 14:29:19 +0700

    Update usdc.sol

--- contract/testnet/usdc.sol
+++ contract/testnet/usdc.sol
@@ -7,10 +7,6 @@ contract usdc is ERC20 {
     constructor() public ERC20("Fake USDC", "USDC") {
     }
 
-    function decimals() public view virtual override returns (uint8) {
-        return 6;
-    }
-
     function mintToken(uint _amount) public {
         _mint(msg.sender, _amount);
     }
